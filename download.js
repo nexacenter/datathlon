6e34518ee738380f98ae6defb13555dafcc10d86
alemela <alessiom92@gmail.com>
2015-09-04 17:06:15 +0200

    Added possibility to be exported

--- download.js
+++ download.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var csv = require('csv');
 
@@ -7,29 +8,31 @@ var output_path = "cordis/";
 var output_filename = "";
 var log_filename ="log.txt";
 
-fs.readFile(input_filename, "utf8", function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    csv.parse(data, function(err, rows) {
-        for (var i = 0; i < rows.length; i++) {
-            var host = rows[i][0];
-            var path = rows[i][1];
-            var match = [];
-            var c = 0;
-            if (match = findRange(rows[i][2])) {
-                for (var j = match[1]; j <= match[2]; j++) {
-                    var code = match[0].replace(/\[.*\]/,"");
-                    trigger(i+c, host, path, j+code);
-                    c++;
-                }
-            } else {
-                var code = rows[i][2];
-                trigger(i, host, path, code);
-            }
+var main = function(){
+    fs.readFile(input_filename, "utf8", function(err, data) {
+        if (err) {
+            return console.log(err);
         }
+        csv.parse(data, function(err, rows) {
+            for (var i = 0; i < rows.length; i++) {
+                var host = rows[i][0];
+                var path = rows[i][1];
+                var match = [];
+                var c = 0;
+                if (match = findRange(rows[i][2])) {
+                    for (var j = match[1]; j <= match[2]; j++) {
+                        var code = match[0].replace(/\[.*\]/,"");
+                        trigger(i+c, host, path, j+code);
+                        c++;
+                    }
+                } else {
+                    var code = rows[i][2];
+                    trigger(i, host, path, code);
+                }
+            }
+        });
     });
-});
+}
 
 function findRange(string) {
     var regexp = /.*\[([0-9]+)\-([0-9]+)\].*/g;
@@ -70,4 +73,27 @@ function getData(options, cb) {
 
 function getNow() {
     return new Date().toISOString();
+}
+
+exports.getFromURL = function (url, cb) {
+    var data = {};
+    data.payload = "";
+
+    var protocol = (url.substring(0, 6).toUpperCase() == 'HTTPS:' ? https : http);
+    var request = protocol.get(url, function(res) {
+        res.on('data', function(chunk) {
+            data.payload += chunk;
+        });
+        res.on('end', function() {
+            data.status = res.statusCode;
+            cb(data);
+        })
+    });
+    request.on('error', function(e) {
+        console.log("Got error: " + e.message + " for " + url);
+    });
+};
+
+if (require.main === module) {
+    main();
 }
