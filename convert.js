b6e88b1d6b8e2a99e1e5db3be81ea354776ca1be
giuseppefutia <giuseppe.futia@gmail.com>
2015-09-09 21:55:32 +0200

    callback function

--- convert.js
+++ convert.js
@@ -12,41 +12,29 @@ var parser = new xml2js.Parser();
 var output = './';
 
 module.exports = (function () {
-    /**
-     * Convert in JSON all XML files located in a specific folder
-     *
-     * @param   {String}    inputDir      Directory of input XML files
-     * @param   {String}    outputDir     Directory of output JSON files
-     *
-     */
-    this.xmlToJson = function (inputDir, outputDir) {
-        fs.readdir(inputDir, function (err, files) {
-        if (err) throw err;
-        files.forEach(function (file) {
-            if (path.extname(file) == ".xml") {
-                fs.readFile(inputDir + file, 'utf-8', function (err, xml) {
-                    if (err) {
-                        console.log(err)
-                    }
-                    createJSON(file, xml, outputDir);
-                });
-            }
+
+    this.publishJSON = function (file, outputDir) {
+        parser.parseString(file, function (err, result) {
+            if (err) throw err;
+            var outputFilename = outputDir + file.split(".xml")[0] + '.json';
+            fs.writeFile(outputFilename, JSON.stringify(result, null, 4), function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("JSON saved to " + outputFilename);
+                }
+            });
         });
-    });
-}
+    }
+
+    this.convertToJSON = function (file, callBack) {
+        fs.readFile(file, 'utf-8', function (err, xml) {
+            if (err) console.log(err)
+            parser.parseString(xml, function (err, result) {
+                if (err) throw err;
+                callBack(result);
+            });
+        });
+    }
     return this;
 })();
-
-var createJSON = function (file, xml, outputDir) {
-    parser.parseString(xml, function (err, result) {
-        if (err) throw err;
-        var outputFilename = outputDir + file.split(".xml")[0] + '.json';
-        fs.writeFile(outputFilename, JSON.stringify(result, null, 4), function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("JSON saved to " + outputFilename);
-            }
-        });
-    });
-}
