b1cc0a3bcc15041c1b18dcd78d64134b8bb0bc0f
giuseppefutia <giuseppe.futia@gmail.com>
2016-01-19 17:39:55 +0100

    String cleaner: manage CDATA[]>

--- cleaner.js
+++ cleaner.js
@@ -8,10 +8,15 @@ module.exports = (function() {
      */
     this.checkBadCharacters = function (string) {
 
+    	// Match <![CDATA[...]]>
+    	var cdataReg = /<!\[CDATA\[.*?\]\]>/;
+
     	// Match everything except a-z, A-Z, 0-9, _
     	var badCharacterReg = /^\W/;
 
     	switch (string) {
+    		case ((string.match(cdataReg)) ? string : undefined):
+    		    return removeCdata(string);
     		case ((string.match(badCharacterReg)) ? string : undefined):
     		    return removeBadCharAtTheBeginning(string);
     		default:
@@ -22,6 +27,11 @@ module.exports = (function() {
     return this;
 
 })();
+
+var removeCdata = function (string) {
+	string = string.replace("<![CDATA[", "").replace("]]>", "");
+	return string;
+}
 
 var removeBadCharAtTheBeginning = function (string) {
     string = string.replace(string[0], "");
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
  * Datathlon test
  * 
  */
- 
+
 var assert = require("assert");
 var stringCleaner = require(__dirname + "/../cleaner.js");
 
@@ -12,5 +12,8 @@ var stringCleaner = require(__dirname + "/../cleaner.js");
 describe("String cleaning test", function () {
     it("Invalid string: input *abcd should return abcd", function () {
         assert.deepEqual("abcd", checkBadCharacters("*abcd"));
+    });
+    it("Invalid string: input <![CDATA[abcd]]> should return abcd", function () {
+        assert.deepEqual("abcd", checkBadCharacters("<![CDATA[abcd]]>"));
     });
 });
