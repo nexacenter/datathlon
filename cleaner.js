e890f6f30032ece0b4ed98262f802d7793c3cf14
giuseppefutia <giuseppe.futia@gmail.com>
2016-01-19 18:31:44 +0100

    String cleaner: manage html tags

--- cleaner.js
+++ cleaner.js
@@ -1,39 +1,47 @@
 module.exports = (function() {
-    
-   /** 
-     * Check bad characters in string
+
+    /** 
+     * Check bad characters in a string
      *
      * @param   {String}       string               String to check
      *
      */
-    this.checkBadCharacters = function (string) {
+    this.cleanString = function(string) {
 
-    	// Match <![CDATA[...]]>
-    	var cdataReg = /<!\[CDATA\[.*?\]\]>/;
+        // Match <![CDATA[...]]>
+        var cdataReg = /<!\[CDATA\[.*?\]\]>/;
 
-    	// Match everything except a-z, A-Z, 0-9, _
-    	var badCharacterReg = /^\W/;
+        // Match html tags
+        var htmlReg = /.*(<([^>]+)>).*/;
 
-    	switch (string) {
-    		case ((string.match(cdataReg)) ? string : undefined):
-    		    return removeCdata(string);
-    		case ((string.match(badCharacterReg)) ? string : undefined):
-    		    return removeBadCharAtTheBeginning(string);
-    		default:
+        // Match everything except a-z, A-Z, 0-9, _
+        var badCharacterReg = /^\W/;
+
+        switch (string) {
+            case ((string.match(cdataReg)) ? string : undefined):
+                return removeCDATA(string);
+            case ((string.match(htmlReg)) ? string : undefined):
+                return removeHTMLTags(string);
+            case ((string.match(badCharacterReg)) ? string : undefined):
+                return removeBadCharAtTheBeginning(string);
+            default:
                 return string;
         }
     }
-
     return this;
-
 })();
 
-var removeCdata = function (string) {
-	string = string.replace("<![CDATA[", "").replace("]]>", "");
-	return string;
+var removeHTMLTags = function(string) {
+    string = string.replace(/(<([^>]+)>)/g, "");
+    return string;
 }
 
-var removeBadCharAtTheBeginning = function (string) {
+var removeCDATA = function(string) {
+    string = string.replace("<![CDATA[", "").replace("]]>", "");
+    return string;
+}
+
+var removeBadCharAtTheBeginning = function(string) {
     string = string.replace(string[0], "");
-	return string;
+    return string;
 }
--- test/test.js
+++ test/test.js
@@ -10,10 +10,13 @@ var stringCleaner = require(__dirname + "/../cleaner.js");
 // String cleaner test
 
 describe("String cleaning test", function () {
-    it("Invalid string: input *abcd should return abcd", function () {
-        assert.deepEqual("abcd", checkBadCharacters("*abcd"));
-    });
     it("Invalid string: input <![CDATA[abcd]]> should return abcd", function () {
-        assert.deepEqual("abcd", checkBadCharacters("<![CDATA[abcd]]>"));
+        assert.deepEqual("abcd", cleanString("<![CDATA[abcd]]>"));
+    });
+    it("Invalid string: input <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"width:263px\"><tbody><tr><td style=\"height:104px; width:263px\">abcd should return abcd", function () {
+        assert.deepEqual("abcd", cleanString("<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"width:263px\"><tbody><tr><td style=\"height:104px; width:263px\">abcd"));
+    });
+    it("Invalid string: input *abcd should return abcd", function () {
+        assert.deepEqual("abcd", cleanString("*abcd"));
     });
 });
