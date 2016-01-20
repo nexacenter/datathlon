8da39e58e9581289edba73757630a5c5e5a2ba2b
giuseppefutia <giuseppe.futia@gmail.com>
2016-01-20 12:32:31 +0100

    Improve string regex with @alemela :rocket:

--- cleaner.js
+++ cleaner.js
@@ -6,7 +6,9 @@ module.exports = (function() {
      * @param   {String}       string               String to check
      *
      */
-    this.cleanString = function(string) {
+    this.cleanString = function (string) {
+
+    	//console.log(string);
 
         // Match <![CDATA[...]]>
         var cdataReg = /<!\[CDATA\[.*?\]\]>/;
@@ -14,34 +16,57 @@ module.exports = (function() {
         // Match html tags
         var htmlReg = /.*(<([^>]+)>).*/;
 
-        // Match everything except a-z, A-Z, 0-9, _
-        var badCharacterReg = /^\W/;
+        // Match everything at the beginning except a-z, A-Z, 0-9, _
+        var badCharBeginningReg = /^\W/;
+
+        // Match everything at the end except a-z, A-Z, 0-9, _, ., !, ?, €, %
+        var badCharEndReg = /[^\w\.?!€%]$/;
+
+        // Match (), [], @
+        var goodCharsReg = /^(\(|\[|@).*/
 
         switch (string) {
             case ((string.match(cdataReg)) ? string : undefined):
-                return removeCDATA(string);
+                string = removeCDATA(string);
+                string = cleanString(string);
+                break;
             case ((string.match(htmlReg)) ? string : undefined):
-                return removeHTMLTags(string);
-            case ((string.match(badCharacterReg)) ? string : undefined):
-                return removeBadCharAtTheBeginning(string);
+                string = removeHTMLTags(string);
+                string = cleanString(string);
+                break;
+            case ((string.match(badCharEndReg)) ? string : undefined):
+                string = removeBadCharAtTheEnd(string);
+                string = cleanString(string);
+                break;
+            case ((string.match(goodCharsReg)) ? string : undefined):
+                return string;
+            case ((string.match(badCharBeginningReg)) ? string : undefined):
+                string = removeBadCharAtTheBeginning(string);
+                string = cleanString(string);
+                break;
             default:
                 return string;
-        }
+        } return string;
     }
     return this;
 })();
 
-var removeHTMLTags = function(string) {
+var removeHTMLTags = function (string) {
     string = string.replace(/(<([^>]+)>)/g, "");
     return string;
 }
 
-var removeCDATA = function(string) {
+var removeCDATA = function (string) {
     string = string.replace("<![CDATA[", "").replace("]]>", "");
     return string;
 }
 
-var removeBadCharAtTheBeginning = function(string) {
+var removeBadCharAtTheBeginning = function (string) {
     string = string.replace(string[0], "");
     return string;
+}
+
+var removeBadCharAtTheEnd = function (string) {
+	string = string.replace(string[string.length-1], "");
+	return string;
 }
--- test/test.js
+++ test/test.js
@@ -10,13 +10,43 @@ var stringCleaner = require(__dirname + "/../cleaner.js");
 // String cleaner test
 
 describe("String cleaning test", function () {
+    it("Valid string: input (abcd) efg should return (abcd) efg", function () {
+        assert.deepEqual("(abcd) efg", cleanString("(abcd) efg"));
+    });
+    it("Valid string: input [abcd] efg should return [abcd] efg", function () {
+        assert.deepEqual("[abcd] efg", cleanString("[abcd] efg"));
+    });
+    it("Valid string: input @abcd should return @abcd", function () {
+        assert.deepEqual("@abcd", cleanString("@abcd"));
+    });
+    it("Invalid string: input \'\' should return false", function () {
+        assert.deepEqual(false, cleanString(""));
+    });
+
+    var badChars = ["+", "‒", "–", "—", "―", ":", "\n", "\r", "*", "!", "?", "/", "\\", ".", "#", "%", "=", "~"];
+    for (var i in badChars) {
+        it("Invalid string: input " + badChars[i] + "abcd should return abcd", function () {
+            assert.deepEqual("abcd", cleanString(badChars[i] + "abcd"));
+        });
+    }
     it("Invalid string: input <![CDATA[abcd]]> should return abcd", function () {
         assert.deepEqual("abcd", cleanString("<![CDATA[abcd]]>"));
+    });
+    it("Invalid string: input <h1>abcd</h1> should return abcd", function () {
+        assert.deepEqual("abcd", cleanString("<h1>abcd</h1>"));
     });
     it("Invalid string: input <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"width:263px\"><tbody><tr><td style=\"height:104px; width:263px\">abcd should return abcd", function () {
         assert.deepEqual("abcd", cleanString("<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"width:263px\"><tbody><tr><td style=\"height:104px; width:263px\">abcd"));
     });
-    it("Invalid string: input *abcd should return abcd", function () {
-        assert.deepEqual("abcd", cleanString("*abcd"));
+});
+
+describe("Pipeline cleaning test", function () {
+    it("Invalid string: input *abcd# should return abcd", function () {
+        var cleanedString = cleanString("*abcd#");
+        assert.deepEqual("abcd", cleanedString);
+    });
+    it("Invalid string: input *   abcd  § # should return abcd", function () {
+        var cleanedString = cleanString("*   abcd  § #");
+        assert.deepEqual("abcd", cleanedString);
     });
 });
